7fb1b95fbb1d45233fc0f80b18acb692f5cd8f33
diogopnt <pintodiogo@ua.pt>
2024-10-26 14:45:30 +0100

    Components

--- todoListFrontend/src/app/app.routes.ts
+++ todoListFrontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MasterComponent } from './shared/layouts/master/master.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: MasterComponent,
+        children: [{path: 'todo', component: TodoComponent }]
+    }
+];
